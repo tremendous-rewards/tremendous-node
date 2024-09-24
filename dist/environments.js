5ea54abdd63fd0095154ea3e025e7ba744f804b7
Lucas Mazza <lucas.mazza@tremendous.com>
2024-09-24 15:38:36 -0300

    chore: remove generated code from repo

--- dist/environments.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Environments = void 0;
-exports.Environments = {
-    production: 'https://api.tremendous.com/api/v2',
-    testflight: 'https://testflight.tremendous.com/api/v2',
-};
